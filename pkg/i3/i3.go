f86b98a97f326324a2c83316e6e98f0d8127c8d1
rholder <ray.holder+github@gmail.com>
2019-04-20 18:38:23 -0500

    Refactor i3status JSON parsing loop and tests into another package

---
+++ pkg/i3/i3.go
@@ -0,0 +1,95 @@
+package i3
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/rholder/i3status-title-on-bar/pkg/window"
+)
+
+func scannerError(out io.Writer, scanner *bufio.Scanner, errorCode int) int {
+	if scanner.Err() != nil {
+		fmt.Fprintf(out, "ERROR from bufio.Scanner: %s\n", scanner.Err())
+	}
+	return errorCode
+}
+
+func newTitleNode(color string, title string) map[string]string {
+	return map[string]string{
+		"name":      "window_title",
+		"full_text": title,
+		"color":     color}
+}
+
+func RunJsonParsingLoop(stdin io.Reader, stdout io.Writer, stderr io.Writer, windowAPI window.WindowAPI, color string) int {
+
+	// read from input
+	scanner := bufio.NewScanner(stdin)
+
+	// Skip the first line which contains the version header.
+	// {"version":1}
+	if !scanner.Scan() {
+		// TODO happens way too often, be more resilient to bad scanner starts from stdin
+		return scannerError(stderr, scanner, 3)
+	}
+	line := strings.TrimSpace(scanner.Text())
+	fmt.Fprintf(stdout, "%s\n", line)
+
+	// The second line contains the start of the infinite array.
+	// [
+	if !scanner.Scan() {
+		return scannerError(stderr, scanner, 4)
+	}
+	line = strings.TrimSpace(scanner.Text())
+	fmt.Fprintf(stdout, "%s\n", line)
+
+	// Start the main loop.
+	var parsed []interface{}
+	prefix := ""
+	for scanner.Scan() {
+		// read from stdin
+		line = strings.TrimSpace(scanner.Text())
+		prefix = ""
+
+		// clip off the comma if it exists, save it to add back later
+		if strings.HasPrefix(line, ",") {
+			line, prefix = line[1:], ","
+		}
+
+		// parse the original JSON
+		err := json.Unmarshal([]byte(line), &parsed)
+		if err != nil {
+			fmt.Fprintln(stderr, err)
+			return 5
+		}
+
+		// inject window title node first
+		title := windowAPI.ActiveWindowTitle()
+		titleNode := newTitleNode(color, title)
+
+		// bolt together the JSON
+		var allJson []interface{}
+		allJson = append(allJson, titleNode)
+		allJson = append(allJson, parsed...)
+
+		// parsed = append(titleNode, parsed...) // TODO figure out how to do this cleanly
+		parsedJson, err := json.Marshal(allJson)
+		if err != nil {
+			fmt.Fprintln(stderr, err)
+			return 6
+		}
+
+		// output i3bar JSON
+		fmt.Fprintf(stdout, "%s%s\n", prefix, parsedJson)
+	}
+
+	if scanner.Err() != nil {
+		return scannerError(stderr, scanner, 7)
+	} else {
+		// we hit EOL normally, everything is fine
+		return 0
+	}
+}
---
+++ pkg/i3/i3_test.go
@@ -0,0 +1,71 @@
+package i3
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+type TestWindowAPI struct{}
+
+func (testWindowAPI TestWindowAPI) ActiveWindowTitle() string {
+	return "foo"
+}
+
+func (testWindowAPI TestWindowAPI) BeginTitleChangeDetection(stderr io.Writer, onChange func()) error {
+	return nil
+}
+
+func TestJsonParsingLoopEmptyInput(t *testing.T) {
+	lines := strings.NewReader("")
+	errorCode := RunJsonParsingLoop(lines, nil, nil, nil, "#00FF00")
+	if errorCode != 3 {
+		t.Fatal("Expected error from parsing loop")
+	}
+}
+
+func TestJsonParsingLoopNewlineInput(t *testing.T) {
+	lines := strings.NewReader("\n")
+	stdout := os.Stdout
+	stderr := os.Stderr
+	errorCode := RunJsonParsingLoop(lines, stdout, stderr, nil, "#00FF00")
+	if errorCode != 4 {
+		t.Fatal("Expected error from parsing loop")
+	}
+}
+
+func TestJsonParsingLoopHappyBlankInput(t *testing.T) {
+	lines := strings.NewReader("\n\n")
+	stdout := os.Stdout
+	stderr := os.Stderr
+	errorCode := RunJsonParsingLoop(lines, stdout, stderr, nil, "#00FF00")
+	if errorCode != 0 {
+		t.Fatal("Expected no error from parsing loop")
+	}
+}
+
+func TestJsonParsingLoopBadJSONInput(t *testing.T) {
+	lines := strings.NewReader("\n\nPOTATO")
+	stdout := os.Stdout
+	stderr := os.Stderr
+	errorCode := RunJsonParsingLoop(lines, stdout, stderr, nil, "#00FF00")
+	if errorCode != 5 {
+		t.Fatal("Expected error from parsing loop")
+	}
+}
+
+func TestJsonParsingLoopGoodJSONInput(t *testing.T) {
+	input := "\n\n" +
+		`[{"name":"wireless","instance":"wlp1s0","color":"#00FF00","markup":"none","full_text":"W: SOME_WIFI_SSID 067%"}]` +
+		"\n" +
+		`,[{"name":"wireless","instance":"wlp1s0","color":"#00FF00","markup":"none","full_text":"W: SOME_WIFI_SSID 064%"}]`
+	lines := strings.NewReader(input)
+	stdout := os.Stdout
+	stderr := os.Stderr
+	windowAPI := TestWindowAPI{}
+	errorCode := RunJsonParsingLoop(lines, stdout, stderr, windowAPI, "#00FF00")
+	if errorCode != 0 {
+		t.Fatal("Expected no error from parsing loop")
+	}
+}
