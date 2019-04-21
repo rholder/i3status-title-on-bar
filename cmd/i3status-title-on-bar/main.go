23e6f853fa49d08667078cb31ef86db0b7fe1fd3
rholder <ray.holder+github@gmail.com>
2019-04-21 17:40:04 -0500

    Add flag parsing for color, version, help, appending to the right, add tests for parsing

--- cmd/i3status-title-on-bar/main.go
+++ cmd/i3status-title-on-bar/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"syscall"
 
@@ -11,10 +14,82 @@ import (
 	"github.com/rholder/i3status-title-on-bar/pkg/window"
 )
 
+const version = "1.0.0"
+const helpText = `Usage: i3status-title-on-bar [OPTIONS...]
+
+  Use i3status-title-on-bar to prepend the currently active X11 window title
+  to the beginning (left) of the i3status output JSON as a new node. From there,
+  i3status should be able to pick it up and display it on the bar.
+
+Options:
+  --color [i3_color_code]  Set the text color of the JSON node (Defaults to #00FF00)
+  --append-end             Append window title JSON node to the end instead of the beginning
+  --fixed-width [integer]  Truncate and pad to a fixed width, useful with append-end
+  --help                   Print this help text and exit
+  --version                Print the version and exit
+
+Examples:
+  i3status | i3status-title-on-bar --color #00EE00
+  i3status | i3status-title-on-bar --append-right --fixed-width 64
+  i3status-title-on-bar < i3status-output-example.json
+
+Report bugs and find the latest updates at https://github.com/rholder/i3status-title-on-bar.
+`
+
+type Config struct {
+	color      string
+	appendEnd  bool
+	fixedWidth int
+	printHelp  bool
+	printVersion bool
+}
+
+func newConfig(name string, args []string) (*Config, error) {
+	fs := flag.NewFlagSet(name, flag.ContinueOnError)
+	var (
+		color = fs.String("color", "#00FF00", "Set the text color of the JSON node")
+		appendEnd = fs.Bool("append-end", false, "Append window title JSON node to the end")
+		fixedWidth = fs.Int("fixed-width", 0, "Trucate and pad to a fixed width")
+		printHelp = fs.Bool("help", false, "Print additional help text and exit")
+		printVersion = fs.Bool("version", false, "Print the version and exit")
+	)
+	// disable default output
+	fs.SetOutput(ioutil.Discard)
+	err := fs.Parse(args)
+
+	return &Config{*color, *appendEnd, *fixedWidth, *printHelp, *printVersion}, err
+}
+
+func shouldExit(stdout io.Writer, config *Config, err error) (bool, int) {
+	if err != nil {
+		fmt.Fprintln(stdout, err.Error() + "\n")
+		fmt.Fprintln(stdout, helpText)
+		return true, 2
+	}
+
+	if config.printHelp {
+		fmt.Fprintln(stdout, helpText)
+		return true, 1
+	}
+
+	if config.printVersion {
+		fmt.Fprintln(stdout, version)
+		return true, 1
+	}
+
+	return false, 0
+}
+
 func main() {
 	stdin := os.Stdin
 	stdout := os.Stdout
 	stderr := os.Stderr
+
+	config, err := newConfig(os.Args[0], os.Args[1:])
+	exit, code := shouldExit(stdout, config, err)
+	if exit {
+		os.Exit(code)
+	}
 
 	titleChangeEvents := make(chan interface{}, 1000)
 
@@ -36,8 +111,6 @@ func main() {
 		}
 	})
 
-	// TODO make color a flag
-	color := "#00FF00"
-	exitCode := i3.RunJsonParsingLoop(stdin, stdout, stderr, windowAPI, color)
+	exitCode := i3.RunJsonParsingLoop(stdin, stdout, stderr, windowAPI, config.color)
 	os.Exit(exitCode)
 }
--- cmd/i3status-title-on-bar/main_test.go
+++ cmd/i3status-title-on-bar/main_test.go
@@ -1,9 +1,165 @@
 package main
 
 import (
+	"io/ioutil"
 	"testing"
 )
 
-func TestCLI(t *testing.T) {
-	// TODO test cli flags, etc.
+func TestCliNoArgs(t *testing.T) {
+	args := []string{}
+	config, err := newConfig("test", args)
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+	if config.color != "#00FF00" {
+		t.Fatal("Expected default color")
+	}
+	if config.appendEnd {
+		t.Fatal("Unexpected appendEnd default")
+	}
+	if config.fixedWidth != 0 {
+		t.Fatal("Unexpected fixedWidth default")
+	}
+	if config.printHelp {
+		t.Fatal("Unexpected printHelp default")
+	}
+	if config.printVersion {
+		t.Fatal("Unexpected printVersion default")
+	}
+
+	exit, code := shouldExit(ioutil.Discard, config, err)
+	if exit {
+		t.Fatal("Unexpected exit")
+	}
+	if code != 0 {
+		t.Fatal("Unexpected exit code")
+	}
 }
+
+func TestCliVersionArgs(t *testing.T) {
+	args := []string{"--version"}
+	config, err := newConfig("test", args)
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+	if config.color != "#00FF00" {
+		t.Fatal("Expected default color")
+	}
+	if config.appendEnd {
+		t.Fatal("Unexpected appendEnd default")
+	}
+	if config.fixedWidth != 0 {
+		t.Fatal("Unexpected fixedWidth default")
+	}
+	if config.printHelp {
+		t.Fatal("Unexpected printHelp default")
+	}
+	if !config.printVersion {
+		t.Fatal("Unexpected printVersion")
+	}
+
+	exit, code := shouldExit(ioutil.Discard, config, err)
+	if !exit {
+		t.Fatal("Unexpected exit")
+	}
+	if code == 0 {
+		t.Fatal("Unexpected exit code")
+	}
+}
+
+func TestCliHelpArgs(t *testing.T) {
+	args := []string{"--help"}
+	config, err := newConfig("test", args)
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+	if config.color != "#00FF00" {
+		t.Fatal("Expected default color")
+	}
+	if config.appendEnd {
+		t.Fatal("Unexpected appendEnd default")
+	}
+	if config.fixedWidth != 0 {
+		t.Fatal("Unexpected fixedWidth default")
+	}
+	if !config.printHelp {
+		t.Fatal("Unexpected printHelp")
+	}
+	if config.printVersion {
+		t.Fatal("Unexpected printVersion default")
+	}
+
+	exit, code := shouldExit(ioutil.Discard, config, err)
+	if !exit {
+		t.Fatal("Unexpected exit")
+	}
+	if code == 0 {
+		t.Fatal("Unexpected exit code")
+	}
+}
+
+func TestCliBadArgs(t *testing.T) {
+	args := []string{"--bad-args"}
+	config, err := newConfig("test", args)
+	if err == nil {
+		t.Fatal("Expected error")
+	}
+	if config.color != "#00FF00" {
+		t.Fatal("Expected default color")
+	}
+	if config.appendEnd {
+		t.Fatal("Unexpected appendEnd default")
+	}
+	if config.fixedWidth != 0 {
+		t.Fatal("Unexpected fixedWidth default")
+	}
+	if config.printHelp {
+		t.Fatal("Unexpected printHelp default")
+	}
+	if config.printVersion {
+		t.Fatal("Unexpected printVersion default")
+	}
+
+	exit, code := shouldExit(ioutil.Discard, config, err)
+	if !exit {
+		t.Fatal("Unexpected exit")
+	}
+	if code == 0 {
+		t.Fatal("Unexpected exit code")
+	}
+}
+
+/*
+
+type Config struct {
+	color      string
+	appendEnd  bool
+	fixedWidth int
+	printHelp  bool
+	printVersion bool
+}
+
+func TestCliNoArgs(t *testing.T) {
+	args := []string{}
+	in := strings.NewReader("")
+	err := cli(args, in, ioutil.Discard, ioutil.Discard)
+	if err == nil {
+		t.Fatal("Expected an error")
+	} else {
+		if err.Error() != "Invalid number of arguments." {
+			t.Fatal("Expected argument error")
+		}
+	}
+}
+
+func TestCliArgs(t *testing.T) {
+	args := []string{"aaa"}
+	in := strings.NewReader("potato\naaa\nmeep")
+	err := cli(args, in, ioutil.Discard, ioutil.Discard)
+	if err != nil {
+		t.Fatal("Unexpected error")
+	}
+
+	// TODO add more tests to verify counts
+}
+*/
