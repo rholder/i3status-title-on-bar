1510c156900ff65c57e691c523d43f7abdafbd2b
rholder <ray.holder+github@gmail.com>
2019-04-21 17:54:32 -0500

    Remove commented out reference notes from test

--- cmd/i3status-title-on-bar/main_test.go
+++ cmd/i3status-title-on-bar/main_test.go
@@ -128,38 +128,3 @@ func TestCliBadArgs(t *testing.T) {
 		t.Fatal("Unexpected exit code")
 	}
 }
-
-/*
-
-type Config struct {
-	color      string
-	appendEnd  bool
-	fixedWidth int
-	printHelp  bool
-	printVersion bool
-}
-
-func TestCliNoArgs(t *testing.T) {
-	args := []string{}
-	in := strings.NewReader("")
-	err := cli(args, in, ioutil.Discard, ioutil.Discard)
-	if err == nil {
-		t.Fatal("Expected an error")
-	} else {
-		if err.Error() != "Invalid number of arguments." {
-			t.Fatal("Expected argument error")
-		}
-	}
-}
-
-func TestCliArgs(t *testing.T) {
-	args := []string{"aaa"}
-	in := strings.NewReader("potato\naaa\nmeep")
-	err := cli(args, in, ioutil.Discard, ioutil.Discard)
-	if err != nil {
-		t.Fatal("Unexpected error")
-	}
-
-	// TODO add more tests to verify counts
-}
-*/
