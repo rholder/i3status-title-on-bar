d09443354df276e9e4c4b9d851a5be5113a24a85
rholder <ray.holder+github@gmail.com>
2019-04-27 15:22:44 -0500

    Add test for SignalPidsWithUSR1

--- pkg/process/process_test.go
+++ pkg/process/process_test.go
@@ -1,9 +1,24 @@
 package process
 
 import (
+	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"testing"
+	"time"
 )
+
+func poll(events chan os.Signal) *os.Signal {
+	select {
+	case msg := <-events:
+		// next message
+		return &msg
+	default:
+		// nil when there is no next message
+		return nil
+	}
+}
 
 func TestFindPidsByProcessName(t *testing.T) {
 	cmd := exec.Command("sleep", "2")
@@ -27,5 +42,26 @@ func TestFindPidsByProcessNameNoProcessExists(t *testing.T) {
 	pids := FindPidsByProcessName("FAKE_PROCESS_NAME")
 	if len(pids) > 0 {
 		t.Fatal("Expected no process ids")
+	}
+}
+
+func TestSignalPidsWithUSR1(t *testing.T) {
+	// attempt to catch a signal from this current process
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGUSR1)
+	currentPid := os.Getpid()
+
+	SignalPidsWithUSR1([]int{currentPid})
+
+	// poll for this signal a few times, sometimes it takes a bit
+	expectedSignal := poll(signalChannel)
+	for i := 0; i < 3; i++ {
+		if expectedSignal == nil {
+			time.Sleep(2 * time.Second)
+			expectedSignal = poll(signalChannel)
+		}
+	}
+	if expectedSignal == nil {
+		t.Fatal("Expected to receive signal USR1")
 	}
 }
