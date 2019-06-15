decf676b81705b07417fe6f133408f804ea2691a
rholder <ray.holder+github@gmail.com>
2019-06-15 16:55:00 -0500

    Refactor and minimize xproto usage in x11.go

--- pkg/window/x11.go
+++ pkg/window/x11.go
@@ -58,51 +58,57 @@ func NewX11() (*X11, error) {
 	}, nil
 }
 
-func (x11 X11) subscribeToActiveWindowChangeEvents() error {
-	// get the currently active windowId
-	reply, err := xproto.GetProperty(x11.XConnection, false, x11.RootWindow, x11.ActiveWindowAtom,
-		xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
-	if err != nil {
-		return err
-	}
-	windowId := xproto.Window(xgb.Get32(reply.Value))
-
-	// subscribe this XConnection to changes in window attributes, like the title attribute
-	xproto.ChangeWindowAttributes(x11.XConnection, windowId,
-		xproto.CwEventMask,
-		[]uint32{ // values must be in the order defined by the protocol
-			xproto.EventMaskStructureNotify |
-				xproto.EventMaskPropertyChange})
-	return nil
-}
-
-func (x11 X11) ActiveWindowTitle() string {
+// Get the currently active window.
+func (x11 X11) activeWindow() (*xproto.Window, error) {
 	// Get the actual value of _NET_ACTIVE_WINDOW.
 	reply, err := xproto.GetProperty(x11.XConnection, false, x11.RootWindow, x11.ActiveWindowAtom,
 		xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
 	if err != nil {
-		// no title on error
-		return ""
+		return nil, err
 	}
-	windowId := xproto.Window(xgb.Get32(reply.Value))
-
-	// Now get the value of _NET_WM_NAME for the active window.
-	reply, err = xproto.GetProperty(x11.XConnection, false, windowId, x11.WindowNameAtom,
-		xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
-	if err != nil {
-		// no title on error
-		return ""
-	}
-	return string(reply.Value)
+	window := xproto.Window(xgb.Get32(reply.Value))
+	return &window, nil
 }
 
-func (x11 X11) BeginTitleChangeDetection(onChange func(), onError func(error)) error {
-	// subscribe to events from the root window
-	xproto.ChangeWindowAttributes(x11.XConnection, x11.RootWindow,
+// Get the title of the given window.
+func (x11 X11) windowTitleProperty(window xproto.Window) (*string, error) {
+	// Now get the value of _NET_WM_NAME for the active window.
+	reply, err := xproto.GetProperty(x11.XConnection, false, window, x11.WindowNameAtom,
+		xproto.GetPropertyTypeAny, 0, (1<<32)-1).Reply()
+	if err != nil {
+		return nil, err
+	}
+	title := string(reply.Value)
+	return &title, nil
+}
+
+func (x11 X11) subscribeToWindowChangeEvents(window xproto.Window) {
+	// subscribe this XConnection to changes in window attributes, like the title attribute
+	xproto.ChangeWindowAttributes(x11.XConnection, window,
 		xproto.CwEventMask,
 		[]uint32{ // values must be in the order defined by the protocol
 			xproto.EventMaskStructureNotify |
 				xproto.EventMaskPropertyChange})
+}
+
+func (x11 X11) ActiveWindowTitle() string {
+	activeWindow, err := x11.activeWindow()
+	if err != nil {
+		// no title on error
+		return ""
+	}
+
+	windowTitle, err := x11.windowTitleProperty(*activeWindow)
+	if err != nil {
+		// no title on error
+		return ""
+	}
+	return *windowTitle
+}
+
+func (x11 X11) BeginTitleChangeDetection(onChange func(), onError func(error)) error {
+	// subscribe to events from the root window
+	x11.subscribeToWindowChangeEvents(x11.RootWindow)
 
 	// Start the main event loop.
 	// TODO refactor this to remove the infinite loop
@@ -127,11 +133,14 @@ func (x11 X11) BeginTitleChangeDetection(onChange func(), onError func(error)) e
 				case x11.WindowNameAtom, x11.WindowName2Atom, x11.WindowName3Atom:
 					onChange()
 				case x11.ActiveWindowAtom:
-					// subscribe to events of all windows as they are activated
 					onChange()
-					err := x11.subscribeToActiveWindowChangeEvents()
+
+					// subscribe to events of all windows as they are activated
+					activeWindow, err := x11.activeWindow()
 					if err != nil {
 						onError(err)
+					} else {
+						x11.subscribeToWindowChangeEvents(*activeWindow)
 					}
 				default:
 					// ignore everything else
